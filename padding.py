44f857e009eab65341e106683df9c4d8f851a6df
eggh234 <61628697+eggh234@users.noreply.github.com>
2024-04-09 18:03:11 -0400

    Add files via upload

--- padding.py
+++ padding.py
@@ -1,47 +1,33 @@
-from frequency import frequency
+#!/usr/bin/env python3
+
+import struct
+import math
+import random
+from frequency import *
+from collections import Counter
 
 
 def padding(artificial_payload, raw_payload):
-    """
-    Append the byte with the largest byte frequency difference to raw_payload.
-    If a byte is only in raw_payload, its frequency is used as the difference.
-    Each call to this function pads only one byte at a time.
-    """
-    # Calculate the frequency of each byte in both payloads
-    artificial_freq = frequency(artificial_payload)
-    raw_freq = frequency(raw_payload)
+    padding = ""
 
-    # Determine the byte with the largest difference
-    max_diff = -1
-    padding_byte = None
+    # Get frequency of raw_payload and artificial profile payload
+    artificial_frequency = frequency(artificial_payload)
+    raw_payload_frequency = frequency(raw_payload)
 
-    # Check each byte in the raw_payload
-    for byte, raw_byte_freq in raw_freq.items():
-        artificial_byte_freq = artificial_freq.get(byte, 0)
+    max = 0
+    padding_byte = ""
+    # Loop through all the keys and values in the raw_payload_frequency
+    # dict. For each key that exists in artificial_payload ,  find the difference in
+    # frequencies. Get the max frequency and append that to raw_payload
+    for key, value in raw_payload_frequency.items():
+        if key in artificial_frequency:
+            artificial_freq = artificial_frequency[key]
+            diff = value - artificial_freq
+            if diff > max:
+                padding_byte = key
+                max = diff
 
-        # If the byte is not in artificial_payload, use the raw_payload frequency
-        diff = (
-            raw_byte_freq
-            if byte not in artificial_freq
-            else artificial_byte_freq - raw_byte_freq
-        )
-
-        # Find the byte with the max diff to use for padding
-        if diff > max_diff:
-            max_diff = diff
-            padding_byte = byte
-
-    # Ensure padding_byte is a byte for appending
-    padding_byte = (
-        padding_byte if isinstance(padding_byte, bytes) else bytes([padding_byte])
-    )
-
-    # Pad the raw_payload if it's shorter than the artificial_payload
-    if len(raw_payload) < len(artificial_payload):
-        # Append one byte of padding
-        raw_payload += padding_byte
-
-    return raw_payload
+    raw_payload.append(padding_byte)
 
     # To simplify padding, you only need to find the maximum frequency difference for each
     # byte in raw_payload and artificial_payload, and pad that byte at the end of the
--- substitution.py
+++ substitution.py
@@ -8,77 +8,67 @@ import numpy
 from collections import Counter
 from frequency import *
 
+
 def substitute(attack_payload, substitution_table):
     # Using the substitution table you generated to encrypt attack payload
     # Note that you also need to generate a xor_table which will be used to decrypt
     # the attack_payload
     # i.e. (encrypted attack payload) XOR (xor_table) = (original attack payload)
-    #b_attack_payload = bytearray(attack_payload, "utf8")
-
+    # b_attack_payload = bytearray(attack_payload, "utf8")
 
     result = []
     xor_table = []
-    #loop through all characters in the attaack payload
+    # loop through all characters in the attaack payload
     for i in range(len(attack_payload)):
         list_sub = substitution_table[attack_payload[i]]
         sub_prob_list = {}
-        replace_char_list = [];
+        replace_char_list = []
         replace_char_prob_list = []
-        #if the attack payload only has a match of 1 list in the sub table, then
-        #use that as the substitute and add to the result and xor list
+        # if the attack payload only has a match of 1 list in the sub table, then
+        # use that as the substitute and add to the result and xor list
         if len(list_sub) == 1:
-            temp  = (list_sub[0])[0];
+            temp = (list_sub[0])[0]
             result.append(temp)
             or1 = ord(attack_payload[i])
             or2 = ord(temp)
-            final_xord  = or1 ^ or2
+            final_xord = or1 ^ or2
             xor_table.append(chr(final_xord))
 
-        #otherwise calculate the weights of each value in the
-        #mapping and randomly select one to enter into the xor and result table
+        # otherwise calculate the weights of each value in the
+        # mapping and randomly select one to enter into the xor and result table
         else:
-            total = 0;
-            #get total weight
+            total = 0
+            # get total weight
             for j in range(len(list_sub)):
                 total += (list_sub[j])[1]
             for x in range(len(list_sub)):
-                sub_prob_list[(list_sub[x])[0]] = ((list_sub[x])[1])/total
+                sub_prob_list[(list_sub[x])[0]] = ((list_sub[x])[1]) / total
                 replace_char_list.append((list_sub[x])[0])
-                replace_char_prob_list.append((list_sub[x])[1]/total)
-                
-            #total_prob=0
-            #only used to verify normalization = 1
-            #for w in sub_prob_list.values():
+                replace_char_prob_list.append((list_sub[x])[1] / total)
+
+            # total_prob=0
+            # only used to verify normalization = 1
+            # for w in sub_prob_list.values():
             #    total_prob += w;
             ##
 
-            #make  a selection from the mapping based on its probablity
-            random_val = numpy.random.choice(a=replace_char_list,p=replace_char_prob_list)
+            # make  a selection from the mapping based on its probablity
+            random_val = numpy.random.choice(
+                a=replace_char_list, p=replace_char_prob_list
+            )
             result.append(random_val)
             or1 = ord(attack_payload[i])
             or2 = ord(random_val)
-            final_xord  = or1 ^ or2
-            xor_table.append(chr(final_xord))     
-                
-
-    print("final result list is " + str(result))
-    print("final xor list is " + str(xor_table))
-
-    print("len result " + str(len(result)) + " len xor " + str(len(xor_table)))
-
+            final_xord = or1 ^ or2
+            xor_table.append(chr(final_xord))
 
     return (xor_table, result)
+
 
 def getSubstitutionTable(artificial_payload, attack_payload):
     # You will need to generate a substitution table which can be used to encrypt the attack
     # body by replacing the most frequent byte in attack body by the most frequent byte in
     # artificial profile one by one
-
-
-    ## TEST for small frequency
-    #artificial_payload = 'abbcccdddd' 
-    #attack_payload = 'rrsss' 
-
 
     # Note that the frequency for each byte is provided below in dictionay format.
     # Please check frequency.py for more details
@@ -88,79 +78,81 @@ def getSubstitutionTable(artificial_payload, attack_payload):
     sorted_artificial_frequency = sorting(artificial_frequency)
     sorted_attack_frequency = sorting(attack_frequency)
 
+    # number of distint characters in attack traffic
+    attack_len = len(sorted_attack_frequency)
 
-    #number of distint characters in attack traffic
-    attack_len = len(sorted_attack_frequency);
+    # number of distincy characters in  normal/artificial
+    normal_len = len(sorted_artificial_frequency)
 
-    #number of distincy characters in  normal/artificial
-    normal_len = len(sorted_artificial_frequency);
-
-
-    temp_sub_table = sorted_attack_frequency;
-    temp_values = [[] for i in range(attack_len)]  #initialize list of attack_len elements
+    temp_sub_table = sorted_attack_frequency
+    temp_values = [
+        [] for i in range(attack_len)
+    ]  # initialize list of attack_len elements
 
     for i in range(attack_len):
         temp_values[i].append(sorted_artificial_frequency[i])
 
-
     substitution_table = {}
     for i in range(len(temp_sub_table)):
-        temp_total = temp_sub_table[i]   #ex ('t',.44) , need to get t and place in new table
-        temp_key= temp_total[0];
+        temp_total = temp_sub_table[
+            i
+        ]  # ex ('t',.44) , need to get t and place in new table
+        temp_key = temp_total[0]
         temp_val = temp_total[1]
-        substitution_table[temp_key]=temp_values[i]
+        substitution_table[temp_key] = temp_values[i]
 
-
-
-    values_left = normal_len-attack_len;
+    values_left = normal_len - attack_len
+    # Loop through the remaining values in the artifical payload
     for j in range(values_left):
         temp_list_comparison = {}
         largest_ration = 0
-        largest_ration_key = '';
-        largest_ration_value = 0;
+        largest_ration_key = ""
+        largest_ration_value = 0
 
         for i in range(attack_len):
-            #get the original frequency/ divide by new frequency
+            # get the original frequency/ divide by new frequency
             original_freq = (sorted_attack_frequency[i])[1]
-            original_key =  (sorted_attack_frequency[i])[0]
-            total =0;
+            original_key = (sorted_attack_frequency[i])[0]
+            total = 0
+            # Get the total frequency
             for k in range(len(substitution_table[original_key])):
                 total += ((substitution_table[original_key])[k])[1]
 
-            new_freq = total;
-            comparison = round(original_freq/new_freq,3);
-            if comparison > largest_ration :
+            new_freq = total
+            comparison = round(original_freq / new_freq, 3)
+            if comparison > largest_ration:
                 largest_ration_key = original_key
-                largest_ration_value = comparison;
-                largest_ration = comparison;
+                largest_ration_value = comparison
+                largest_ration = comparison
 
-            temp_list_comparison[original_key] = comparison;
+            temp_list_comparison[original_key] = comparison
 
-
-
-        substitution_table[largest_ration_key].append(sorted_artificial_frequency[attack_len +j])
+        substitution_table[largest_ration_key].append(
+            sorted_artificial_frequency[attack_len + j]
+        )
 
     # Make sure your substitution table can be used in
     print(substitution_table)
-    #substitute(attack_payload, substitution_table)
+    # substitute(attack_payload, substitution_table)
 
     return substitution_table
 
 
 def getAttackBodyPayload(path):
-    f = open(path, 'rb')
+    f = open(path, "rb")
     pcap = dpkt.pcap.Reader(f)
     for ts, buf in pcap:
         eth = dpkt.ethernet.Ethernet(buf)
         ip = eth.data
-        if socket.inet_ntoa(ip.dst) == "192.150.11.111": 
+        if socket.inet_ntoa(ip.dst) == "192.150.11.111":
             tcp = ip.data
             if tcp.data == "":
                 continue
             return tcp.data.rstrip()
 
+
 def getArtificialPayload(path):
-    f = open(path, 'rb')
+    f = open(path, "rb")
     pcap = dpkt.pcap.Reader(f)
     for ts, buf in pcap:
         eth = dpkt.ethernet.Ethernet(buf)
